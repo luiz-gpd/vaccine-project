91ad484cbf3cf6f1b73549041026903be549e06f
Luiz Pompílio <luiz.pompilio412@gmail.com>
2021-04-16 19:32:41 -0300

    Primeiro teste, Page component

---
+++ frontend/src/__tests__/components/Page/page.spec.js
@@ -0,0 +1,28 @@
+import Page from "../../../components/Page";
+
+import { render } from "@testing-library/react";
+
+describe("Page Component", () => {
+  it("renders", () => {
+    const { asFragment } = render(<Page />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("render with props", () => {
+    const { queryByText } = render(
+      <Page title="Título">
+        <div className="pokemon--list">
+          <span>Uno</span>
+          <span>Due</span>
+          <span>Tre</span>
+        </div>
+      </Page>
+    );
+
+    expect(queryByText("Título")).toBeTruthy();
+    expect(queryByText("Uno")).toBeTruthy();
+    expect(queryByText("Due")).toBeTruthy();
+    expect(queryByText("Tre")).toBeTruthy();
+  });
+});
